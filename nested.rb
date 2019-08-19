919d2b7367fc3548965a7fddd8ae51116168be56
Levi Borland <borland.levi@gmail.com>
2019-08-19 05:55:10 +0000

    Automatically backed up by Learn

--- nested.rb
+++ nested.rb
@@ -85,12 +85,10 @@ def adding_matz
         }
      }
 
-    programmer_hash[:yukihiro_matsumoto => {
-      known_for: "Ruby",
-      languages: ["LISP", "C"]
-    }]
-    
-    return programmer_hash
+   programmer_hash[:yukihiro_matsumoto] = {
+     known_for: "Ruby",
+     languages: ["LISP", "C"]
+   }
 end
 
 def changing_alan
